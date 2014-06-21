62602ea7a18ece8f34f5ff27f7a30949cc60d078
Johan Brook <johan.jvb@gmail.com>
2014-06-21 15:56:26 +0200

    Refactor + Comments

--- site.js
+++ site.js
@@ -62,10 +62,18 @@ var render = function(string, data) {
 
 // Create a template function bound to a
 // template string ready for rendering data
+// Usage: var tmpl = template('Hi {name}')
+//        var rendered = tmpl({name: 'Johan'})
+//        => 'Hi Johan'
 var template = function(string) {
   return render.bind(this, string)
 }
 
+// Matches:
+//   Svårighetsgrad: <level>
+//   **Svårighetsgrad: **<level>
+//   Svårighetsgrad <level>
+//   etc.
 var extractLevel = function(content) {
   var matches = content.match(/Svårighetsgrad[\W\s]*(\d)/i)
   return matches ? matches[1] : false
@@ -73,17 +81,20 @@ var extractLevel = function(content) {
 
 // Create a plain object for templating
 var transformExercise = function(exercise, i) {
+  // Convert encoded README content to regular text
   var raw = base64ToUTF8(exercise.data.content),
+      // Convert Markdown -> HTML with the Marked library
       content = marked(raw)
 
   return {
     order: i,
     level: extractLevel(raw) || 'Okänd',
-    text: $(content).eq(0).text(),
+    text: $(content).eq(0).text(),      // The first heading
     content: content
   }
 }
 
+// Comparator function for exercises
 var sortByLevel = function(exercises) {
   return exercises.sort(function(e1, e2) {
     return (e1.level > e2.level) ? 1 :
@@ -101,30 +112,41 @@ var renderReadmeList = function(exercises) {
   return $container.html( exercises.map(tmpl).join("") )
 }
 
+// Custom function mimicing Q.all():
+// returns a promise whose argument is
+// an array with all data objects. Takes
+// and array of promises
+var whenAll = function(promises) {
+  var slice = Array.prototype.slice
+  return $.when.apply($, promises).then(function() {
+    return slice.call(arguments).map(function(r) {
+      // The first element is the data object
+      // The others are the status and the XHR promise itself.
+      return r[0]
+    })
+  })
+}
+
 // Fetch READMEs and build list
 
 var fetchReadmeas = function() {
-  var self = this,
-      slice = Array.prototype.slice,
-
-      whenAll = function(promises) {
-        return $.when.apply($, promises)
-      }
+  var self = this
 
   GitHub
+    // Fetch exercises from the GitHub API
     .getExercises()
+    // Then for each exercise, fetch its README
     .then(function(exercises){
+      // Return a promise when all README-fetches have settled
       return whenAll( exercises.map(GitHub.getReadmeForExercise.bind(GitHub)) )
-      .then(function() {
-        return slice.call(arguments).map(function(r) {
-          return r[0]
-        })
-      })
     })
+    // Transform each exercise (parse out relevant data for templating)
     .then(function(exercises) {
       return exercises.map(transformExercise)
     })
+    // Sort by difficulty level
     .then(sortByLevel)
+    // Render the exercises with README content
     .then(renderReadmeList)
 
 }
